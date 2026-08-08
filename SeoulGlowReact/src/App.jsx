6d8069917a81b66177062549609a5ccceea87f6d
Yukki <stevenangeloroquesalazar@gmail.com>
2026-08-08 15:46:57 -0500

    prueba

--- SeoulGlowReact/src/App.jsx
+++ SeoulGlowReact/src/App.jsx
@@ -1,16 +1,14 @@
 import './App.css';
-import Header from "./componentes/Header.jsx";
 import Home from "./paginas/Home.jsx";
-import Footer from "./componentes/Footer.jsx";
+
 
 
 
 function App() {
     return (
         <div>
-            <Header />
+
             <Home />
-            <Footer />  
 
         </div>
     );
